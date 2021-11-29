fc9154f5284d667d3564617befd08e19fa1ca261
Juho Rissanen <bgp040@myy.haaga-helia.fi>
2021-11-29 20:18:21 +0200

    Fixed some errors after pull - push

--- RuntimeTerror/src/main/java/Project/RuntimeTerror/domain/Question.java
+++ RuntimeTerror/src/main/java/Project/RuntimeTerror/domain/Question.java
@@ -1,9 +1,9 @@
 package Project.RuntimeTerror.domain;
-<<<<<<< HEAD
+
 import java.util.List;
 
 import javax.persistence.*;
-=======
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,14 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
->>>>>>> 980550e3b66fdcedca63592d575816fc628b82a2
+
 
 @Entity
 public class Question {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-<<<<<<< HEAD
 	private Long questionId;
 	private String questionType;
 	private String question;
@@ -33,90 +32,80 @@ public class Question {
 	@OneToMany(cascade = CascadeType.ALL)
 	private List<Answer> answers;
 	
-	public Question() {}
-
-	public Question(String questionType, String question, List<Option> options) {
-		super();
-=======
-	private Long id;
-	private String title;
-
-	@ManyToOne
-	@JoinColumn(name = "questionTypeId")
-	private QuestionType questionType;
-
-	@ManyToOne
-	@JoinColumn(name = "optionId")
-	private Options option;
 
 	public Question() {
 	}
 
-	public Question(String title, QuestionType questionType, Options option) {
+
+	public Question(String questionType, String question, List<Option> options) {
 		super();
-		this.title = title;
->>>>>>> 980550e3b66fdcedca63592d575816fc628b82a2
 		this.questionType = questionType;
 		this.question = question;
 		this.options = options;
 	}
-<<<<<<< HEAD
-	
+
+
 	public Question(String questionType, String question) {
 		super();
-=======
-
-	public Question(String title, QuestionType questionType) {
-		super();
-		this.title = title;
->>>>>>> 980550e3b66fdcedca63592d575816fc628b82a2
 		this.questionType = questionType;
 		this.question = question;
 	}
-	
+
+
 	public Long getQuestionId() {
 		return questionId;
 	}
+
 
 	public void setQuestionId(Long questionId) {
 		this.questionId = questionId;
 	}
 
+
 	public String getQuestionType() {
 		return questionType;
 	}
+
 
 	public void setQuestionType(String questionType) {
 		this.questionType = questionType;
 	}
 
-	public String getTitle() {
-		return title;
+
+	public String getQuestion() {
+		return question;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+
+	public void setQuestion(String question) {
+		this.question = question;
 	}
+
 
 	public List<Option> getOptions() {
 		return options;
 	}
 
+
 	public void setOptions(List<Option> options) {
 		this.options = options;
 	}
+
 
 	public Questionnaire getQuestionnaire() {
 		return questionnaire;
 	}
 
+
 	public void setQuestionnaire(Questionnaire questionnaire) {
 		this.questionnaire = questionnaire;
 	}
 
+
 	public List<Answer> getAnswers() {
 		return answers;
 	}
+
 
 	public void setAnswers(List<Answer> answers) {
 		this.answers = answers;
